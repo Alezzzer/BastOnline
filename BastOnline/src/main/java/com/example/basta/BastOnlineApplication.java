597c4c7af8dfee1b7f2c14f032b6ef48c34d4bd2
Aleksandar Zeradjanin <alexandarzera@gmail.com>
2025-03-21 14:26:54 +0100

    project created in eclipse

---
+++ BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.basta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastOnlineApplication.class, args);
+	}
+
+}
---
+++ BastOnline/src/test/java/com/example/basta/BastOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
