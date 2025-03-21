6be84d482d829e58bad039ef07a0f16ac3d2112c
Petra Radojkovic <radojkovicpetra.it@outlook.com>
2025-03-21 14:44:09 +0100

    dodato jos nesto

--- BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
+++ BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BastOnlineApplication {
 
-//	Blablablabla
+//	Blablablabla dodato
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BastOnlineApplication.class, args);
