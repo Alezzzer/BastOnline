5ee0496912a560c5999f28cf9cc1bee913d96afa
Aleksandar Zeradjanin <alexandarzera@gmail.com>
2025-03-21 14:34:42 +0100

    komentar

--- BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
+++ BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BastOnlineApplication {
 
+//	Blablablabla
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BastOnlineApplication.class, args);
 	}
