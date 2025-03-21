a771a63fbd15309f39e6786fba761908033236d7
Petra Radojkovic <radojkovicpetra.it@outlook.com>
2025-03-21 15:34:48 +0100

    Add properties for database and minor changes.

--- BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
+++ BastOnline/src/main/java/com/example/basta/BastOnlineApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BastOnlineApplication {
 
-//	Blablablabla dodato
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BastOnlineApplication.class, args);
