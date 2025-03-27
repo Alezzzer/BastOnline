253bd61f69860347924b50b58e34d87b2e1f5f29
Aleksandar Zeradjanin <alexandarzera@gmail.com>
2025-03-27 14:39:27 +0100

    Revert "Merge branch 'main' into HEAD"
    This reverts commit b388c077adbf50c280ef9819d29105af361f30b2, reversing
    changes made to 3e79fb81baae0ad887867a6de66a89eaf8610d2e.

--- BastOnline/src/main/java/com/example/basta/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.basta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.basta.entity.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long>{
-
-}
--- BastOnline/src/main/java/com/example/basta/repository/OrderRepository.java
+++ BastOnline/src/main/java/com/example/basta/repository/OrderRepository.java
@@ -1,11 +1,9 @@
 package com.example.basta.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.basta.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
-	  List<Order> findByUserId(Long userId);
+
 }
