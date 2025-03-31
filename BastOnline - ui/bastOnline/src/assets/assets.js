c9822c4f19cb43b61dde5e27ba66c8385ad988a1
Aleksandar Zeradjanin <alexandarzera@gmail.com>
2025-03-31 17:49:59 +0200

    added header

--- BastOnline - ui/bastOnline/src/assets/assets.js
+++ BastOnline - ui/bastOnline/src/assets/assets.js
@@ -2,7 +2,41 @@
 import logo from './logo.png'
 import searchIcon from './search-icon.png'
 import shoppingBasket  from './shopping_basket.png'
+import header from './header-img.png'
 export const assets = {
-    logo, searchIcon, shoppingBasket
+    logo, searchIcon, shoppingBasket, header
 }
 
+// export const menu_list = [
+//     {
+//         menu_name: "Salad",
+//         menu_image: menu_1
+//     },
+//     {
+//         menu_name: "Rolls",
+//         menu_image: menu_2
+//     },
+//     {
+//         menu_name: "Deserts",
+//         menu_image: menu_3
+//     },
+//     {
+//         menu_name: "Sandwich",
+//         menu_image: menu_4
+//     },
+//     {
+//         menu_name: "Cake",
+//         menu_image: menu_5
+//     },
+//     {
+//         menu_name: "Pure Veg",
+//         menu_image: menu_6
+//     },
+//     {
+//         menu_name: "Pasta",
+//         menu_image: menu_7
+//     },
+//     {
+//         menu_name: "Noodles",
+//         menu_image: menu_8
+//     }]
